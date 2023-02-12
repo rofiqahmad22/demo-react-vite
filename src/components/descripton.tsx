f61662d996353a5fa4c647daa8e77dc9f3baff8a
Rofiq Ahmad Mubarok <rofiqahmadm@gmail.com>
2023-02-12 18:48:51 +0700

    create description component

---
+++ src/components/descripton.tsx
@@ -0,0 +1,10 @@
+export function Description() {
+  return (
+    <div>
+      <p>
+        do what you want! don't choose a path that you don't think is right and
+        regret it, there's no slowing down!
+      </p>
+    </div>
+  );
+}
