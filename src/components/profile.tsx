5d98f7a481a872e1d837390b36024f0891b56ef9
Rofiq Ahmad Mubarok <rofiqahmadm@gmail.com>
2023-02-12 18:48:20 +0700

    create profile component

---
+++ src/components/profile.tsx
@@ -0,0 +1,10 @@
+export function Profile() {
+  return (
+    <div>
+      <h1>My Profil</h1>
+      <p>Name: Rofiq Ahmad Mubarok</p>
+      <p>Age: 24 Years Old</p>
+      <p>Gender: Male</p>
+    </div>
+  );
+}
