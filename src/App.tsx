e850a0cd11a41e3684df8fe5c887717c9366c9af
Rofiq Ahmad Mubarok <rofiqahmadm@gmail.com>
2023-02-12 18:47:24 +0700

    export default function app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/button";
+import { Description } from "./components/descripton";
+import { Profile } from "./components/profile";
+
+export default function App() {
+  return (
+    <div>
+      <Profile />
+      <Description />
+      <Button />
+    </div>
+  );
+}
