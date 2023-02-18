a27ec163d1acb520b41dbe111570362027afb8c7
Rofiq Ahmad Mubarok <rofiqahmadm@gmail.com>
2023-02-18 08:06:25 +0700

    setup tailwindcss and add ListItem

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,33 @@ import { Description } from "./components/descripton";
 import { Profile } from "./components/profile";
 
 export default function App() {
+  const dataProfiles = [
+    {
+      _id: "0",
+      name: "Rofiq Ahmad",
+    },
+    {
+      _id: "1",
+      name: "Rahmat Subarjo",
+    },
+  ];
   return (
     <div>
+      <header>
+        <h1 className="text-3xl font-bold">My Profile</h1>
+      </header>
       <Profile />
       <Description />
       <Button />
+      <ul>
+        {dataProfiles.map((profile) => {
+          return <ListItem key={profile._id} />;
+        })}
+      </ul>
     </div>
   );
+
+  function ListItem() {
+    return <li className="font-bold">Profil</li>;
+  }
 }
